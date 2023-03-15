44ea7617e87a413fcb076c3baeba46f04d137474
PriyaGunjal <118348552+PriyaGunjal@users.noreply.github.com>
2023-03-15 10:18:20 +0000

    simpleEx

---
+++ ExpensesItem.js
@@ -0,0 +1,9 @@
+function ExpensesItem() {
+    return (
+        <div><div>Food rs.10</div>
+            <h2>Petrol rs.100</h2>
+            <div>Movies rs.200</div></div>
+    );
+}
+
+export default ExpensesItem;
