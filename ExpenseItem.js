400c97c5fb2cc82a07d99fc089de461243acfe89
PriyaGunjal <118348552+PriyaGunjal@users.noreply.github.com>
2023-03-28 11:54:08 +0530

    Delete Expense

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,12 +1,16 @@
 import ExpenseDate from "./ExpenseDate";
 
 import "./ExpenseItem.css";
-//import Card from "../UI/Card";
+import Card from "../UI/Card";
 import ExpenseDetail from "./ExpenseDetail";
 
 const ExpenseItem=(props)=> {
+  const deleteExpense=() =>{
+    const expenseItem = document.querySelector('.expense-item');
+    expenseItem.parentNode.removeChild(expenseItem);
+  }
    return (
-          <div className="expense-item">
+          <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
               <h2>{props.title}</h2>
@@ -16,7 +20,8 @@ const ExpenseItem=(props)=> {
                 <h2>{props.locationOfExpenditure}</h2>
               </div>
             </div>
-          </div>
+            <button onClick={deleteExpense}>Delete Expense</button>
+          </Card>
         );
     
     
