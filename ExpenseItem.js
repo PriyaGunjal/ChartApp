72f559e552a2921ce1147fe8267eb06f09c67948
PriyaGunjal <118348552+PriyaGunjal@users.noreply.github.com>
2023-04-24 15:23:09 +0530

    changes in expenseitem

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 
 import "./ExpenseItem.css";
@@ -6,26 +6,19 @@ import Card from "../UI/Card";
 import Expenses from "./Expenses";
 
 const ExpenseItem=(props)=> {
- const [title,setTitle]= useState(props.title);
-  
  
-  const clickHandler=()=>{
-   setTitle('updated!')
-    console.log(title);
-  };
-  
    return (
           <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-              <h2>{title}</h2>
+              <h2>{props.title}</h2>
               <div className="expense-item__price">${props.amount}</div>
               <Expenses amount={props.amount} />
               <div className="expense-item__description">
                 <h2>{props.locationOfExpenditure}</h2>
               </div>
             </div>
-            <button onClick={clickHandler}>change title</button>
+            
           </Card>
         );
     
